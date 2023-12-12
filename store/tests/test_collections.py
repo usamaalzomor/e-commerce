5a5a9c6ed7439c31f218d8874cb448e133bddf6a
Usamaalzomor <osama.zomor2010@gmail.com>
2023-12-12 10:04:16 +0200

    test(store): add test for retrieving a collction with 200 ok status
    description:
    - this test checks that retrieving a collection returns a 200 ok status along with the correct data.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
+from model_bakery import baker
+from store.models import Collection
 
 @pytest.fixture
 def create_collection(api_client):
@@ -39,3 +41,17 @@ class TestCreateCollection:
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0                                   
+        }
